20ac444d380938340d7a13d3a2a7d7f31b73b4d9
Patryk Ferenc <patrykferenc@protonmail.com>
2025-10-19 17:30:45 +0200

    add old kan model

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,12 @@
 from .sfnnv9 import model as SFNNv9
 from .sfnnvkan_g2_k2_3072_15_32_mlp import model as SFNNvkan_g2_k2_3072_15_32_mlp
 from .sfnnvkan_g3_k3_3072_15_32_mlp import model as SFNNvkan_g3_k3_3072_15_32_mlp
+from .sfnnvkan_early_3072_15_32 import model as SFNNvkan_early_3072_15_32
 
 nets = {
     'sfnnv9': lambda feature_set: SFNNv9.NNUE(feature_set),
     'sfnnvkan_g2_k2_3072_15_32_mlp': lambda feature_set: SFNNvkan_g2_k2_3072_15_32_mlp.NNUE(feature_set),
     'sfnnvkan_g3_k3_3072_15_32_mlp': lambda feature_set: SFNNvkan_g3_k3_3072_15_32_mlp.NNUE(feature_set),
+    'sfnnvkan_early_3072_15_32': lambda feature_set: SFNNvkan_early_3072_15_32.NNUE(feature_set),
 }
 """nets contains models to be run in scripts"""
